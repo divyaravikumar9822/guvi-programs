8196382c6a0fd939729d4c97ec716aa2ef411b22
divyaravikumar9822 <30408986+divyaravikumar9822@users.noreply.github.com>
2017-09-20 20:59:40 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int base,exp;
+long long res;
+printf("enter the base:");
+scanf("%d",&base);
+printf("enter the exp:");
+scanf("%d",&exp);
+while(exp!=0)
+{
+res*=base;
+--exp;
+}
+printf("answer=%lld",res);
+return 0;
+}
